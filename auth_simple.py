38e66b58e178cae9a600fc8d6c40d9fc6aa705dc
luchamorro <chamorro.lucia@gmail.com>
2025-06-02 21:27:24 +0200

    hash

--- auth_simple.py
+++ auth_simple.py
@@ -1,17 +1,15 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash
 import os
+import bcrypt
+
 
 simple_auth_bp = Blueprint('simple_auth', __name__)
 
-# Contraseña encriptada (ejemplo: hash de "admin123")
-HASHED_PASSWORD = os.environ.get('ADMIN_PASSWORD_HASH', '32768:8:1$nbjlk8Tz9gr1qssr$4264eba5a1851b24eb265416ab4fdac982b706e349e5a0b7e6662b414de0d35830f7dd55fae549c375231d65b868526fd689bd79b44dc670db9ca04e93c7aaf0.')
+stored_hash = b'$2b$12$zbq08OGuHEQe.mbmo.iknubfPLfjB0q/RSCX1hSIuDXJvPS.r0UZm'
 
 @simple_auth_bp.route('/verify-password', methods=['POST'])
-def verify_password():
-    from flask import request, jsonify
-    import bcrypt
-
+def _password():
     try:
         data = request.get_json()
         print("Datos recibidos:", data)
@@ -19,9 +17,6 @@ def verify_password():
         password = data.get('password')
         if not password:
             return jsonify({"error": "No se proporcionó contraseña"}), 400
-
-        # Hash estático de la contraseña "admin123" (solo para ejemplo)
-        stored_hash = b'32768:8:1$nbjlk8Tz9gr1qssr$4264eba5a1851b24eb265416ab4fdac982b706e349e5a0b7e6662b414de0d35830f7dd55fae549c375231d65b868526fd689bd79b44dc670db9ca04e93c7aaf0.'
 
         if bcrypt.checkpw(password.encode('utf-8'), stored_hash):
             return jsonify({"access": True}), 200
