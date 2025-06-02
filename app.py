fa6fb31cffa4a03b22fe713ccd1788ec0fbbe92a
luchamorro <chamorro.lucia@gmail.com>
2025-06-02 22:38:07 +0200

    static y cambios en app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from controllers.markers import markers_bp
 from auth_simple import simple_auth_bp
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -11,3 +12,13 @@ app.register_blueprint(simple_auth_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def serve_login():
+    return send_from_directory('static', 'login.html')
+
+@app.route('/admin_panel.html')
+def serve_admin_panel():
+    return send_from_directory('static', 'admin_panel.html')
